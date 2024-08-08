f4c868eb2102a94874bf6d1526fbd2937dcfb44b
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 11:00:36 -0300

    Consumindo API coinap

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,63 @@
-import { useState , FormEvent } from 'react'
+import { useState , FormEvent, useEffect } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from  'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
 
+interface CoinProps{
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr:string;
+  explorer: string;
+}
+
+interface DataProp{
+  data: CoinProps[]
+}
+
 export function Home() {
   const [input, setInput] = useState("")
+  const [coins, setCoins] = useState<CoinProps[]>([]);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  async function getData(){
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+    .then(response => response.json())
+    .then((data: DataProp) => {
+      const coinsData = data.data;
+
+      const price = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      })
+
+      const formatedResult = coinsData.map((item) => {
+        const formated = {
+          ...item,
+          formatedPrice: price.format(Number(item.priceUsd)),
+          formatedMarket: price.format(Number(item.marketCapUsd))
+        }
+
+        return formated;
+      })
+
+      console.log(formatedResult);
+
+    })
+
+  }
 
   function handleSubmit(e: FormEvent){
     e.preventDefault();
