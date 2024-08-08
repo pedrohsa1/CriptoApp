a349686ba8a24fa5148b201b8292c3c4607cc830
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 13:55:46 -0300

    Pagina de detalhes

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,8 +1,71 @@
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { CoinProps } from '../home'
+
+interface ResponseData {
+  data: CoinProps
+}
+
+interface ErrorData {
+  error: string;
+}
+
+type DataProps = ResponseData | ErrorData
 
 export function Detail() {
+  const { cripto } = useParams();
+  const navigate = useNavigate();
+
+  const [coin, setCoin] = useState<CoinProps>()
+
+  useEffect(() => {
+    async function getCoin(){
+      try{
+        fetch(`https://api.coincap.io/v2/assets/${cripto}`)
+        .then(response => response.json())
+        .then((data: DataProps) => {
+          
+          if("error" in data){
+            navigate("/")
+            return;
+          }
+
+          const price = Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+          })
+    
+          const priceCompact = Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            notation: "compact"
+          })
+
+          const resultData = {
+            ...data.data,
+            formatedPrice: price.format(Number(data.data.priceUsd)),
+            formatedMarket: priceCompact.format(Number(data.data.marketCapUsd)),
+            formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr))
+          }
+
+          setCoin(resultData)
+
+        })
+
+
+      }catch(err){
+        console.log(err);
+        navigate("/")
+      } 
+    }
+
+    getCoin();
+  }, [cripto]) 
+
+
   return (
     <div>
-      <h1>Página Detalhe da moeda</h1>
+      <h1>Página Detalhe da moeda {cripto}</h1>
     </div>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import styles from './home.module.css'
 import { BsSearch } from  'react-icons/bs'
 import { Link, useNavigate } from 'react-router-dom'
 
-interface CoinProps{
+export interface CoinProps{
   id: string;
   name: string;
   symbol: string;
