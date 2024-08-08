106034bdea92bbfb2b90e051978cc94d0fa0c4e3
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 13:57:59 -0300

    Exibindo detalhes

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { CoinProps } from '../home'
+import styles from './detail.module.css'
 
 interface ResponseData {
   data: CoinProps
@@ -17,6 +18,7 @@ export function Detail() {
   const navigate = useNavigate();
 
   const [coin, setCoin] = useState<CoinProps>()
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getCoin(){
@@ -49,6 +51,7 @@ export function Detail() {
           }
 
           setCoin(resultData)
+          setLoading(false);
 
         })
 
@@ -63,9 +66,45 @@ export function Detail() {
   }, [cripto]) 
 
 
+  if(loading || !coin){
+    return(
+      <div className={styles.container}>
+        <h4 className={styles.center}>Carregando detalhes...</h4>
+      </div>
+    )
+  }
+
+
   return (
-    <div>
-      <h1>Página Detalhe da moeda {cripto}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.center}>{coin?.name}</h1>
+      <h1 className={styles.center}>{coin?.symbol}</h1>
+
+      <section className={styles.content}>
+        <img
+          src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`}
+          alt="Logo da moeda"
+          className={styles.logo}
+        />
+        <h1>{coin?.name} | {coin?.symbol}</h1>
+
+        <p><strong>Preço: </strong>{coin?.formatedPrice}</p>
+
+        <a>
+          <strong>Mercado: </strong>{coin?.formatedMarket}
+        </a>
+
+        <a>
+          <strong>Volume: </strong>{coin?.formatedVolume}
+        </a>
+
+        <a>
+          <strong>Mudança 24h: </strong><span className={Number(coin?.changePercent24Hr) > 0 ? styles.protift : styles.loss} >{Number(coin?.changePercent24Hr).toFixed(3)}</span>
+        </a>
+
+
+      </section>
+
     </div>
   )
 }
