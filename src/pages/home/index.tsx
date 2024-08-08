c679e706853410cad336205af23ef02bf4df9e20
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 10:51:01 -0300

    Adicionando estilos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,33 @@
+import { useState , FormEvent } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from  'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
+  const [input, setInput] = useState("")
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault();
+
+    if(input === "") return;
+
+    navigate(`/detail/${input}`)
+  }
+
+  function handleGetMore(){
+    alert("TESTE")
+  }
 
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input 
           type="text"
           placeholder="Digite o nome da moeda... EX bitcoin"
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
         />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
@@ -52,7 +70,7 @@ export function Home() {
               2B
             </td>
 
-            <td className={styles.tdLabel} data-label="Mudança 24h">
+            <td className={styles.tdProfit} data-label="Mudança 24h">
               <span>1.20</span>
             </td>
 
@@ -60,6 +78,10 @@ export function Home() {
 
         </tbody>
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
 
     </main>
   )
